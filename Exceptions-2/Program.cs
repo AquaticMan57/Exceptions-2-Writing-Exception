2e5179b24b3e0a21fcbfbacdd567c54863cac1fd
erayyaman5759@gmail.com <erayyaman5759@gmail.com>
2021-04-10 20:34:01 +0300

    Add project files.

---
+++ Exceptions-2/Program.cs
@@ -0,0 +1,44 @@
+﻿using Exceptions_2;
+using System;
+using System.Collections.Generic;
+
+namespace Exceptions_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyClass myclass = new MyClass() { };
+            try
+            {
+                myclass.Function();
+            }
+            catch (RecordNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+                
+            }
+        }
+    }
+    class MyClass : IClass
+    {
+
+        public void Function()
+        {
+            List<string> students = new List<string>() { "Eray", "Anil", "Yaman" };
+            if (!students.Contains("Ahmet"))
+            {
+                throw new RecordNotFoundException("Record not found");
+            }
+            else
+            {
+                Console.WriteLine("Record Found!");
+            }
+        }
+    }
+    interface IClass
+    {
+        void Function();
+    }
+}
